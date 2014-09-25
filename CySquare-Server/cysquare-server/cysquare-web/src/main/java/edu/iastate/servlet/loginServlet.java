c408357c9b907431009528a74e8dd6b04013c03a
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-09-25 17:04:57 +0000

    Updated with psuedo code

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
@@ -14,16 +14,40 @@ public class loginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -6761544027811752080L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) {
-		String username = request.getParameter("username");
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	{
+		//getParameter method returns a string of the JSON Object from the android project
+		String username_string = request.getParameter("username");
+		String password_string = request.getParameter("password");
 		
-		response.setContentType("text/json");	//Check syntax on content types
+		//check if username_string is null or blank
+		
+		//check if password_string is null or blank
+		
+		//Set the content type to JSON for when this sends the response
+		response.setContentType("application/json");
+		
+		//send the username to the dao
+		//receive a pojo from the dao
+		
+		//if there is no username the dao will return a null
+		//then should send a json object with text stating there is no user by that name
+		//else compare passwords
+		
 		JSONObject object = new JSONObject();
-		try {
+		try 
+		{
+			
 			object.write(response.getWriter());
-		} catch (JSONException e) {
+			
+			
+		} 
+		catch (JSONException e) 
+		{
 			e.printStackTrace();
-		} catch (IOException e) {
+		} 
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
