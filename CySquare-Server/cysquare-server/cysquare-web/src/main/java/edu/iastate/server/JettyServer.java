b0b5d8bddd59fdf66a98fff6e7afd7f57150d0fa
lmilius <lmilius@users.noreply.github.com>
2014-09-27 03:47:47 +0000

    Set up Jetty server to run locally for testing. When compiled, a war file is generated. This war file is able to be deployed to the apache server and be ran. Also added the dao module to write daos in.

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/server/JettyServer.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/server/JettyServer.java
@@ -1,11 +1,18 @@
 package edu.iastate.server;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServer {
 
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+		Server server = new Server(8081);
+		
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/");
+		webapp.setWar("./target/cysquare-web-1.0.0-SNAPSHOT.war");
+		server.setHandler(webapp);
 		
 		server.join();
 		server.start();
