4be8fddfd3cc7f1487647a865f16ac465070a264
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-09-28 22:47:05 +0000

    Revised the loginServlet class to include error messages to be sent to the client

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
@@ -14,18 +14,26 @@ public class loginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -6761544027811752080L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
 		//getParameter method returns a string of the JSON Object from the android project
 		String username_string = request.getParameter("username");
 		String password_string = request.getParameter("password");
-		
-		//check if username_string is null or blank
-		
-		//check if password_string is null or blank
-		
+
 		//Set the content type to JSON for when this sends the response
 		response.setContentType("application/json");
+		
+		//check if username_string is null or blank
+		if((username_string == null) || (username_string == ""))
+		{
+			response.sendError(401, "The username is invalid");
+		}
+		//check if password_string is null or blank
+		if((password_string == null) || (password_string == ""))
+		{
+			response.sendError(401, "The password is invalid");
+		}
+
 		
 		//send the username to the dao
 		//receive a pojo from the dao
