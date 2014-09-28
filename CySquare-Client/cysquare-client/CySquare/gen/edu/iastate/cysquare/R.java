a4aaadc0650d4712dd4a09de3d46b6d2f8ee7f2e
kstaines <kstaines@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-09-28 22:37:32 +0000

    Added Login button.

--- CySquare-Client/cysquare-client/CySquare/gen/edu/iastate/cysquare/R.java
+++ CySquare-Client/cysquare-client/CySquare/gen/edu/iastate/cysquare/R.java
@@ -1092,10 +1092,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button_login=0x7f050043;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1206,6 +1207,7 @@ containing a value of this type.
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int Login=0x7f0a0013;
         public static final int Welcome_to_CySquare=0x7f0a0010;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
