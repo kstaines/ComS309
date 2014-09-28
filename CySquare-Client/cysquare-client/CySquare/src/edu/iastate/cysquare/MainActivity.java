8e95c94e96755c74308bd9cc03187ac9512c7004
kstaines <kstaines@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-09-28 23:06:16 +0000

    If the username "admin" and password "admin" are entered, text pops up that says "Logging in." If anything else is entered, text pops up that says "Incorrect username or password."

--- CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/MainActivity.java
+++ CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/MainActivity.java
@@ -4,14 +4,36 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	private EditText username=null;
+	private EditText password=null;
+	private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.editText_username);
+        password = (EditText)findViewById(R.id.editText_password);
+        login = (Button)findViewById(R.id.button_login);
+    }
+    
+    public void login(View view){
+    	if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+    		Toast.makeText(getApplicationContext(), "Logging in...",
+    		Toast.LENGTH_LONG).show();
+    	}
+    	else{
+    		Toast.makeText(getApplicationContext(), "Incorrect username or password", Toast.LENGTH_LONG).show();
+    	}
     }
 
 
