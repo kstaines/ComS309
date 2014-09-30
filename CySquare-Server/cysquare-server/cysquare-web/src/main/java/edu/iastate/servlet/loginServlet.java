9fa7b6a9f88c6322c4dc9f0562775f283dc9e884
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-09-30 18:50:30 +0000

    Used the dao class to get account info. Will fix the error message of importing the packages.

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/loginServlet.java
@@ -3,12 +3,16 @@ package edu.iastate.servlet;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import edu.iastate.domain.UserAccount;
+import edu.iastate.dao.impl.AccountDAO;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 public class loginServlet extends HttpServlet {
 
@@ -27,31 +31,41 @@ public class loginServlet extends HttpServlet {
 		if((username_string == null) || (username_string == ""))
 		{
 			response.sendError(401, "The username is invalid");
+			//or can send this if the error code does not work
+			//response.getWriter().write("The username is invalid");
 		}
 		//check if password_string is null or blank
 		if((password_string == null) || (password_string == ""))
 		{
 			response.sendError(401, "The password is invalid");
+			//or can send this if the error code does not work
+			//response.getWriter().write("The password is invalid");
 		}
 
 		
 		//send the username to the dao
 		//receive a pojo from the dao
-		
+		UserAccount newuser = getAccountInfo(username_string);
+		String pojo_username = newuser.getUsername();
+		String pojo_password = newuser.getPassword();
 		//if there is no username the dao will return a null
 		//then should send a json object with text stating there is no user by that name
-		/*if(pojo_username == null)
+		if(pojo_username == null)
 		{
 			response.sendError(401, "There is no user by that name");
-		}*/
+			//or can send this if the error code does not work
+			//response.getWriter().write("There is no user by that name");
+		}
 		
 		//else compare passwords
-		/*else
+		else
 		{
 			//if the passwords have different lengths then they are not the same
 			if(password_string.length() != pojo_password.length())
 			{
 				response.sendError(401, "The password does not match the username");
+				//or can send this if the error code does not work
+				//response.getWriter().write("The password does not match the username");
 			}
 			for(int i = 0; i < password_string.length(); i++)
 			{
@@ -60,22 +74,16 @@ public class loginServlet extends HttpServlet {
 				if(c_one != c_two)
 				{
 					response.sendError(401, "The password does not match the username");
+					//or can send this if the error code does not work
+					//response.getWriter().write("The password does not match the username");
 				}
-			}
-			 
-			
-			
-		}*/
-		
-		
-		
+			}			
+		}
 		JSONObject object = new JSONObject();
 		try 
 		{
-			
+			object.put("status", true);
 			object.write(response.getWriter());
-			
-			
 		} 
 		catch (JSONException e) 
 		{
---
+++ CySquare-Server/cysquare-server/cysquare-web/src/test/java/edu/iastate/servlet/LoginServletTest.java
@@ -0,0 +1,14 @@
+package edu.iastate.servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginServletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
