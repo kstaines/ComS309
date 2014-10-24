0fe44168ba82419e784220b1ba726915504800b6
snulmer <snulmer@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-10-24 15:28:48 +0000

    Create user uses JSONCommunication class to sendPost to server

--- CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/CreateUser.java
+++ CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/CreateUser.java
@@ -1,32 +1,20 @@
 package edu.iastate.cysquare;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import edu.iastate.cysquare.domain.Parameter;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -144,7 +132,8 @@ public class CreateUser extends ActionBarActivity implements OnItemSelectedListe
 				jo.put("usertype", userType);
 				
 				//Send message and get response
-				String build = sendPost(url, jo);
+				JSONCommunication jc = new JSONCommunication();
+				String build = jc.sendPost(http, request, response, url, jo);
 				
 				return build;
 	    	}
@@ -192,29 +181,6 @@ public class CreateUser extends ActionBarActivity implements OnItemSelectedListe
 		}
     }
 	
-	private String sendPost(String url, JSONObject jo) throws ClientProtocolException, IOException, JSONException {
-		
-		List<Parameter> parameters = new ArrayList<Parameter>();
-		Iterator<String> keys = jo.keys();
-		while(keys.hasNext()) {
-			String key = keys.next();
-			Parameter param = new Parameter();
-			param.setName(key);
-			param.setValue(jo.getString(key));
-			parameters.add(param);
-		}
-		StringEntity mySE = new UrlEncodedFormEntity(parameters);
-		mySE.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/x-www-form-urlencoded;")); //setContentType sets content type of the response being sent to the client
-		request = new HttpPost(url);
-		request.setEntity(mySE);
-
-		response = http.execute(request);
-					
-		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-		String build = reader.readLine();
-		return build;
-	}
-
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
 			long arg3) {
