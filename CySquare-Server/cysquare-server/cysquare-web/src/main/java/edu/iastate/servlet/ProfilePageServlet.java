74ddb1d0d5c253f6a4f60f468e8d144f6f2010b2
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-10-24 01:34:39 +0000

    Edited the ProfilePageServlet.java to get the course information from the dao

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/ProfilePageServlet.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/ProfilePageServlet.java
@@ -1,6 +1,7 @@
 package edu.iastate.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.iastate.dao.impl.AccountDAO;
+import edu.iastate.dao.impl.StudentCourseDAO;
+import edu.iastate.domain.StudentCourses;
 import edu.iastate.domain.UserAccount;
 
 @WebServlet("/profilePage")
@@ -19,6 +22,7 @@ public class ProfilePageServlet extends HttpServlet{
 	private static final long serialVersionUID = -4151895874414506005L;
 	
 	private AccountDAO accountDao = new AccountDAO();
+	private StudentCourseDAO course = new StudentCourseDAO();
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
@@ -61,6 +65,15 @@ public class ProfilePageServlet extends HttpServlet{
 			profile.put("points", points);
 			
 			//course id and user id
+			Integer userId = user.getUserId();
+			
+			//get courses from the student course dao
+			List<StudentCourses> courseList = course.getCourses("" + userId);
+			int [] courseId = new int [courseList.size()];
+			for(int i = 0; i < courseList.size(); i++)
+			{
+				courseId [i]= courseList.get(i).getCourseId();
+			}
 			//a list would be the course id
 			//get the course table with that course id
 		} catch (JSONException e) {
