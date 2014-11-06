dd3ba88d69d7d59c9c395fbca6b43d52b492c654
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-11-06 18:00:56 +0000

    Created a FriendsPageServlet.java class

---
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/FriendsPageServlet.java
@@ -0,0 +1,96 @@
+package edu.iastate.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+@WebServlet("/friendsPage")
+public class FriendsPageServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 3563797253016667883L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		String username = request.getParameter("user");
+		String friendName = request.getParameter("friend");
+		
+		JSONObject friend = new JSONObject ();
+		
+		checkNull(friend, username, "user", response);
+		checkBlank(friend, username, "user", response);
+		
+		//checkNull(friend)
+		
+		
+	}
+	
+	
+
+	
+	private void putError(JSONObject object, String message, HttpServletResponse response)
+	{
+		try 
+		{
+			object.put("status", "error");
+			object.put("error", message);
+			try 
+			{
+				object.write(response.getWriter());
+				return;
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		} 
+		catch (JSONException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void putTrue(JSONObject object, HttpServletResponse response)
+	{
+		try
+		{
+			object.put("status", true);
+			try
+			{
+				object.write(response.getWriter());
+				return;
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private void checkNull(JSONObject object, String toCheck, String toType, HttpServletResponse response)
+	{
+		if(toCheck == null)
+		{
+			putError(object, "The " + toType + " is null.", response);
+		}
+	}
+	
+	private void checkBlank(JSONObject object, String toCheck, String toType, HttpServletResponse response)
+	{
+		if(toCheck.equalsIgnoreCase(""))
+		{
+			putError(object, "The " + toType + " is blank.", response);
+		}
+	}
+}
+
