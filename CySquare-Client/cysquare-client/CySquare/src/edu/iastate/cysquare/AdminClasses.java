93ff9140a4d636362b742c7213b5f9a9c31355ef
snulmer <snulmer@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-11-23 00:04:05 +0000

    AdminClasses page displays list of all classes and a delete button (which does nothing yet)

--- CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/AdminClasses.java
+++ CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/AdminClasses.java
@@ -1,18 +1,33 @@
 package edu.iastate.cysquare;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AdminClasses extends Activity{
 	public static final String PREFS_NAME = "MyPreferencesFile";
 	
 	private Button home;
 	private Intent homeIntent;
+	private final static String classListURL = "http://proj-309-w03.cs.iastate.edu/cysquare-web-1.0.0-SNAPSHOT/classList";
+	private String className, section;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -20,6 +35,10 @@ public class AdminClasses extends Activity{
 		setContentView(R.layout.admin_classes); //sets screen layout
 		
 		home = (Button)findViewById(R.id.home_button);
+		
+		displayAllClasses();
+		registerClick();
+		
 		home.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -51,5 +70,102 @@ public class AdminClasses extends Activity{
     private void goHome(){
     	startActivity(homeIntent);
     }
+    
+    private void displayAllClasses() {
+    	JSONObject jo = new JSONObject();
+    	new PostWithAsync(classListURL, jo).execute();
+    }
+    
+    private void createClassesArrayAndListView(JSONObject response) {
+    	try {
+			int size = response.getInt("size");
+			String[] classesArray = new String[size];
+			for (int i=0; i<size; i++) {
+				String courseKey = "Course";
+				courseKey = courseKey.concat(Integer.toString(i+1));
+				classesArray[i] = response.getString(courseKey);
+			}
+			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listview_items, classesArray);
+			ListView classList = (ListView)findViewById(R.id.allClassesListView);
+			classList.setAdapter(adapter);
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+    }
+    
+	private void registerClick() {
+		ListView classList = (ListView)findViewById(R.id.allClassesListView);
+		classList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+					long id) {
+				TextView tv = (TextView) arg1;
+				getClassNameAndSection(tv.getText().toString());
+				Toast.makeText(AdminClasses.this, className + " is selected", Toast.LENGTH_LONG).show();
+			}
+		});
+		
+	}
+	
+	private void getClassNameAndSection(String courseInfo) {
+		Scanner scan = new Scanner(courseInfo);
+		while (scan.hasNext()) {
+			String scanned = scan.next();
+			if (scanned.equalsIgnoreCase("Name:")) {
+				className = scan.next();
+			}
+			else if (scanned.equalsIgnoreCase("Section:")) {
+				section = scan.next();
+			}
+		}
+		scan.close();
+	}
+    
+
+    private class PostWithAsync extends AsyncTask<String, String, String> {
+		private String url;
+		private JSONObject jo;
+		public PostWithAsync(String url, JSONObject jo) {
+			this.url = url;
+			this.jo = jo;
+		}
+		
+		@Override
+		protected String doInBackground(String... arg0) {
+	    	try{
+				JSONCommunication jc = new JSONCommunication();
+				String build = jc.sendPost(url, jo);
+				return build;
+	    	}
+	    	catch (JSONException e){
+				e.printStackTrace();
+			}
+	    	catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+	    	catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		protected void onPostExecute(String build) {
+			JSONObject responseObject;
+			try {
+				responseObject = new JSONObject(build);
+				if (responseObject.has("status") && responseObject.getString("status").equals("error")) {
+					Toast.makeText(AdminClasses.this, responseObject.getString("error"), Toast.LENGTH_LONG).show();
+				}
+				else {
+					if(url.equals(classListURL)) createClassesArrayAndListView(responseObject);
+				}
+			}
+	    	catch (JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
