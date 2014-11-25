59b048564205076ee6425c95e48b86cf635654a8
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-11-25 21:54:40 +0000

    Created the ResetPageServlet.java

---
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/ResetPageServlet.java
@@ -0,0 +1,70 @@
+package edu.iastate.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+@WebServlet("/resetPage")
+public class ResetPageServlet extends HttpServlet{
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		//No request parameters from the client
+		
+		//Get all the student users
+		//Delete the all the student user points and class list
+		
+	}
+	private void putError(JSONObject object, String message, HttpServletResponse response)
+	{
+		try 
+		{
+			object.put("status", "error");
+			object.put("error", message);
+			try 
+			{
+				object.write(response.getWriter());
+				return;
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		} 
+		catch (JSONException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void putTrue(JSONObject object, HttpServletResponse response)
+	{
+		try
+		{
+			object.put("status", true);
+			try
+			{
+				object.write(response.getWriter());
+				return;
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
