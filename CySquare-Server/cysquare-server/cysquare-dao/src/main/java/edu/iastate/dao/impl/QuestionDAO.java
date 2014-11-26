aa8397a27d17f76550c95ef83b32294dfde02482
lmilius <lmilius@users.noreply.github.com>
2014-11-26 05:24:49 +0000

    Forgot to save a file, this is in continuation from the previous commit with the QuestionDAO.java.

--- CySquare-Server/cysquare-server/cysquare-dao/src/main/java/edu/iastate/dao/impl/QuestionDAO.java
+++ CySquare-Server/cysquare-server/cysquare-dao/src/main/java/edu/iastate/dao/impl/QuestionDAO.java
@@ -8,9 +8,35 @@ import edu.iastate.domain.Question;
 
 public class QuestionDAO extends DatabaseAccess {
 
+	/**
+	 * Retrieve all questions that were added as a particular
+	 * instructor. Search based on the instructor userID to find
+	 * all questions posted by that instructor.
+	 * @param userId
+	 * @return
+	 */
 	public List<Question> getQuestionsFromInstructor(int userId) {
 		
 		return new ArrayList<Question>();
+	}
+	
+	/**
+	 * To create a question, you will need the instructor userID
+	 * and the string of the question.
+	 * @param instructorId
+	 * @param question
+	 */
+	public void createQuestion(int instructorId, String question) {
+		
+	}
+	
+	/**
+	 * To delete a question, you will need the unique question ID
+	 * that is stored in the question object.
+	 * @param questionId
+	 */
+	public void deleteQuestion(int questionId) {
+		
 	}
 	
 }
