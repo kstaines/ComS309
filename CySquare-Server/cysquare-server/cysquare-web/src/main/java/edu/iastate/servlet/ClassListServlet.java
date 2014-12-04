82854d02150be2aed6b7fb237cc4a947c85199a8
blopez11 <blopez11@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-12-04 17:05:14 +0000

    Modified the ClassListServlet.java to include which user's class list would be returned to the client.

--- CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/ClassListServlet.java
+++ CySquare-Server/cysquare-server/cysquare-web/src/main/java/edu/iastate/servlet/ClassListServlet.java
@@ -1,6 +1,7 @@
 package edu.iastate.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import edu.iastate.dao.impl.AccountDAO;
 import edu.iastate.dao.impl.CourseDAO;
+import edu.iastate.dao.impl.InstructorCourseDAO;
 import edu.iastate.domain.Course;
+import edu.iastate.domain.InstructorCourse;
+import edu.iastate.domain.UserAccount;
 @WebServlet("/classList")
 
 public class ClassListServlet extends HttpServlet{
@@ -20,10 +25,12 @@ public class ClassListServlet extends HttpServlet{
 	private static final long serialVersionUID = 8438444817401559341L;
 	
 	private CourseDAO courseDao = new CourseDAO ();
+	private InstructorCourseDAO instructorDao = new InstructorCourseDAO ();
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
 		//get all the request parameters
 		String editType = request.getParameter("editType");
+		
 		
 		JSONObject list = new JSONObject ();
 		
@@ -33,32 +40,36 @@ public class ClassListServlet extends HttpServlet{
 		//Get the corresponding courseList based upon if admin or instructor.
 		if(editType.equalsIgnoreCase("instructor"))
 		{
+			//get the request parameter of username
+			String instructor = request.getParameter("username");
+			AccountDAO accountDao = new AccountDAO ();
+			UserAccount instructorAccount = accountDao.getAccountInfo(instructor);
+			int instructorId = instructorAccount.getUserId();
+			List<InstructorCourse> courseList1 = instructorDao.getCourses(instructorId);
 			
-		}
-		List<Course> courseList = courseDao.getAvailableCourseList();
-		
-		try
-		{
-			if(courseList.isEmpty())
+			//Check if null or empty
+			if(courseList1 == null || courseList1.isEmpty())
 			{
-				putError(list, "There is no class lists in the database.", response);
+				putError(list, "There is no class lists in the database for the instructor.", response);
 				return;
 			}
-			list.put("size", courseList.size());
-			for(int i = 0; i < courseList.size(); i++)
+			//Loop through to get the course information that will be sent to client
+			List<Course> courseList = new ArrayList<Course> ();
+			for(int i = 0; i < courseList1.size(); i++)
 			{
-				Course course = courseList.get(i);
-				list.put("Course" +(i+1), "Course name: " + course.getName() + " Section: " + course.getSection() +
-							 " Location: " + course.getLocation() + " Days: " + course.getDays() + " Time: " + course.getTime());
+				Integer courseId = courseList1.get(i).getCourseId();
+				Course course = courseDao.getCourseInfoById(courseId);
+				courseList.add(course);
 			}
-			list.write(response.getWriter());
+			putList(list, courseList, response);
 			return;
-		}
-		
-		catch (JSONException e)
-		{
 			
-			e.printStackTrace();
+		}
+		else
+		{
+			List<Course> courseList = courseDao.getAvailableCourseList();
+			putList(list, courseList, response);
+			return;
 		}
 	}
 	/*
@@ -109,6 +120,37 @@ public class ClassListServlet extends HttpServlet{
 			return true;
 		}
 		return false;
+	}
+	
+	/*
+	 * Private Helper method to put the list in a JSONObject
+	 * @param list A JSONObject that is sent to the client
+	 * @param courseList A List of courses
+	 * @param response A http servlet response to be sent to the client
+	 */
+	private void putList(JSONObject list, List<Course> courseList, HttpServletResponse response) throws IOException
+	{
+		if(courseList == null || courseList.isEmpty())
+		{
+			putError(list, "There is no class lists in the database.", response);
+			return;
+		}
+		try {
+			list.put("size", courseList.size());
+		
+			for(int i = 0; i < courseList.size(); i++)
+			{
+				Course course = courseList.get(i);
+				
+				list.put("Course" +(i+1), "Course name: " + course.getName() + " Section: " + course.getSection() +
+							 " Location: " + course.getLocation() + " Days: " + course.getDays() + " Time: " + course.getTime());
+			
+			}
+		
+		list.write(response.getWriter());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
