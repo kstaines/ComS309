a72906b5d46dece8674401867893b66f72ac9138
kstaines <kstaines@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-12-07 06:01:07 +0000

    AdminReset.java is done. Haven't tested resetting everything.

--- CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/AdminReset.java
+++ CySquare-Client/cysquare-client/CySquare/src/edu/iastate/cysquare/AdminReset.java
@@ -1,17 +1,28 @@
 package edu.iastate.cysquare;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AdminReset extends Activity{
 	public static final String PREFS_NAME = "MyPreferencesFile";
-
+	private static final String resetPageURL = "http://proj-309-w03.cs.iastate.edu/cysquare-web-1.0.0-SNAPSHOT/resetPage";
 	private Button home;
+	private Button reset;
 	private Intent homeIntent;
 	
 	@Override
@@ -20,6 +31,8 @@ public class AdminReset extends Activity{
 		setContentView(R.layout.admin_reset); //sets screen layout
 		
 		home = (Button)findViewById(R.id.home_button);
+		reset = (Button)findViewById(R.id.reset_button);
+		
 		home.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -27,6 +40,14 @@ public class AdminReset extends Activity{
 				goHome();
 			} //end onClick(view v)
 		});
+		
+		reset.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+		    	resetAlert();
+			} //end onClick(View v)
+		});
+		
 	} //end onCreate(Bundle savedInstanceState)
 	
     @Override
@@ -51,6 +72,77 @@ public class AdminReset extends Activity{
     private void goHome(){
     	startActivity(homeIntent);
     }
+    
+    private void resetAlert(){
+    	AlertDialog.Builder alertDialog = new AlertDialog.Builder(AdminReset.this);
+    	alertDialog.setTitle("Reset All");
+    	alertDialog.setMessage("This will reset all student user points and class lists.\n" +
+    			"Are you sure want to reset all?");
+    	//on pressing no button
+    	alertDialog.setPositiveButton("No", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+    	//on pressing yes button
+    	alertDialog.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				resetAll();
+			}
+		});
+    	alertDialog.show();
+    }
+    
+    private void resetAll(){
+    	JSONObject jo = new JSONObject();
+    	new PostWithAsync(resetPageURL, jo).execute();
+    }
+    
+    private class PostWithAsync extends AsyncTask<String, String, String>{
+    	private String URL;
+    	private JSONObject sendJSONObject;
+    	public PostWithAsync(String resetURL, JSONObject jo){
+    		URL = resetURL;
+    		sendJSONObject = jo;
+    	}
+    	
+    	protected String doInBackground(String... arg0){
+    		try{
+    			JSONCommunication jc = new JSONCommunication();
+    			String build = jc.sendPost(URL,  sendJSONObject);
+    			return build;
+    		}
+    		catch (JSONException e){
+ 				e.printStackTrace();
+ 			}
+ 	    	catch (UnsupportedEncodingException e) {
+ 				e.printStackTrace();
+ 			}
+ 	    	catch (IOException e) {
+ 				e.printStackTrace();
+ 			} 
+			return null;
+    	} //end String doInBackground(String... arg0)
+    	
+    	protected void onPostExecute(String build){
+    		JSONObject responseObject;
+    		try{
+    			responseObject = new JSONObject(build);
+    			if(responseObject.getString("status").equalsIgnoreCase("true")){
+    				Toast.makeText(AdminReset.this, "All student points and class lists have been reset", Toast.LENGTH_LONG).show();
+    			}
+    			else{
+    				Toast.makeText(AdminReset.this, responseObject.getString("error"), Toast.LENGTH_LONG).show();
+
+    			}
+    		}
+    		catch (JSONException e) {
+				e.printStackTrace();
+			}
+    	} //end onPostExecute(String build)
+    } //end PostWithAsync extends AsyncTask<String, String, String>
 
 
 }
